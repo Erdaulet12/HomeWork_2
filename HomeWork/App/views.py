7baaac6867a641e9d085e9df4465335cf76d3bdc
Erdaulet12 <erdauletakilzhan83@gmail.com>
2024-07-10 23:11:23 +0500

    start

--- HomeWork/App/views.py
+++ HomeWork/App/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+	return render(request, 'App/home.html')
+
+
+def login_view(request):
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			return redirect('home')
+		else:
+			messages.error(request, 'Invalid username or password')
+
+	return render(request, 'App/login.html')
