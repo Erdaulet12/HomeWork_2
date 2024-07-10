1e18dd78a097a4ecee7f250866cc6513709dc797
Erdaulet12 <erdauletakilzhan83@gmail.com>
2024-07-10 20:15:49 +0500

    start

--- HomeWork/HomeWork/settings.py
+++ HomeWork/HomeWork/settings.py
@@ -54,6 +54,7 @@ ROOT_URLCONF = 'HomeWork.urls'
 
 TEMPLATES = [
     {
+        'DIRS': [BASE_DIR / "templates"],
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
